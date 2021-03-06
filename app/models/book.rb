ce5a15b4acfd79ac0f45d71145bf23bda46c2439
Karl Heitmann <khf1988@gmail.com>
2021-03-05 23:13:23 -0300

    anotado los atributos del modelo usando la gema annotate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  checkin    :date
+#  checkout   :date
+#  status     :integer
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Book < ApplicationRecord
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  author     :string
+#  checkin    :date
+#  checkout   :date
+#  status     :integer
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
